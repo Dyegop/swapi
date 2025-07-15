987fd6538574fe0f985a1571955f205a8912efd2
diego <27856897+Dyegop@users.noreply.github.com>
2025-07-15 23:06:54 +0200

    feature: Add basic config vars

--- src/core/config.py
+++ src/core/config.py
@@ -7,13 +7,21 @@ import pydantic_settings
 class BaseConfig(pydantic_settings.BaseSettings):
     """Describes app configuration."""
 
+    app_name: str = "swapi"
+    app_host: str = "https://127.0.0.1"
+    app_port: int = 8000
+    reload: bool = False
+
 
 class ResourceConfig(pydantic_settings.BaseSettings):
     """Describes SWAPI resource configuration."""
 
-    swapi_url: pydantic.HttpUrl = "https://swapi.info/api"
-    swapi_people_url: pydantic.HttpUrl = "https://swapi.info/api/people"
-    swapi_planets_url: pydantic.HttpUrl = "https://swapi.info/api/planets"
+    base_url: pydantic.HttpUrl = "https://swapi.info/api"
+    people_url: pydantic.HttpUrl = "https://swapi.info/api/people"
+    planets_url: pydantic.HttpUrl = "https://swapi.info/api/planets"
+
+    default_page_size: int = 10
+    """Default items to return by page."""
 
 
 @lru_cache()
