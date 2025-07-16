b2d7141465727ec4d44c5a8f84878f736b31bb90
diego <27856897+Dyegop@users.noreply.github.com>
2025-07-16 12:44:22 +0200

    feat: Add title and description to model fields

--- src/app/models/base_model.py
+++ src/app/models/base_model.py
@@ -8,11 +8,15 @@ model_field = pydantic.Field
 class WithTimestamps(pydantic.BaseModel):
     """Describes a base model with timestamps."""
 
-    created: datetime.datetime
-    edited: datetime.datetime
+    created: datetime.datetime = model_field(title="Created", description="The resource creation datetime.")
+
+    edited: datetime.datetime = model_field(
+        title="Edited",
+        description="The datetime when the resource was last modified.",
+    )
 
 
 class WithUrl(pydantic.BaseModel):
     """Describes a base model with resource url."""
 
-    url: pydantic.HttpUrl
+    url: pydantic.HttpUrl = model_field(title="Url", description="The resource url.")
