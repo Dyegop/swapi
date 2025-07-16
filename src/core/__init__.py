72b9dc7d5c9f512c5eed20008f4f7f35ed6a5b66
diego <27856897+Dyegop@users.noreply.github.com>
2025-07-16 17:24:32 +0200

    refactor: Revamped config and add new variables

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,2 +1,2 @@
-from src.core.config import get_app_config, get_cli_config, get_resource_config
+from src.core.config import get_app_config, get_resource_config
 from src.core.logger import get_logger
--- src/core/config.py
+++ src/core/config.py
@@ -1,34 +1,33 @@
 from functools import lru_cache
+from urllib.parse import urljoin
 
-import pydantic
 import pydantic_settings
 
 
 class AppConfig(pydantic_settings.BaseSettings):
     """Describes app configuration."""
 
-    app_name: str = "swapi"
-    app_host: str = "127.0.0.1"
-    app_port: int = 8000
+    name: str = "swapi"
+    host: str = "127.0.0.1"
+    port: int = 8000
     reload: bool = False
+
+    @property
+    def url(self) -> str:
+        return f"http://{self.host}:{self.port}"
 
 
 class ResourceConfig(pydantic_settings.BaseSettings):
     """Describes SWAPI resource configuration."""
 
-    base_url: pydantic.HttpUrl = "https://swapi.info/api"
-    people_url: pydantic.HttpUrl = "https://swapi.info/api/people"
-    planets_url: pydantic.HttpUrl = "https://swapi.info/api/planets"
+    people_resource: str = "people"
+    planets_resource: str = "planets"
+    base_url: str = "https://swapi.info/api"
+    people_endpoint: str = urljoin(base_url, people_resource)
+    planets_endpoint: str = urljoin(base_url, planets_resource)
 
     default_page_size: int = 10
     """Default items to return by page."""
-
-
-class CLIConfig(pydantic_settings.BaseSettings):
-    """Describes SWAPI CLI configuration."""
-
-    people_table_title: str = "People"
-    planets_table_title: str = "Planets"
 
 
 @lru_cache()
@@ -41,9 +40,3 @@ def get_app_config() -> AppConfig:
 def get_resource_config() -> ResourceConfig:
     """Returns a cached instance of the resource configuration."""
     return ResourceConfig()
-
-
-@lru_cache()
-def get_cli_config() -> CLIConfig:
-    """Returns a cached instance of the CLI app configuration."""
-    return CLIConfig()
